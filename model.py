c7279a17bfb2691ff51e351851c5fc949bc1b0e0
thisisiron <kimiron518@gmail.com>
2019-04-23 14:39:00 +0900

    define classes of each layer

---
+++ model.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+
+class Embedder(tf.keras.Model):
+    def __init__(self):
+        super(Embedder, self).__init__()
+
+    def call(self, input_tensor):
+
+
+class Encoder(tf.keras.Model):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+    def call(self, x, hidden):
+
+
+class Decoder(tf.keras.Model):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+    def call(self):
+
+class AttentionLayer(tf.keras.Model):
+    def __init__(self):
+        super(Attention, self).__init__()
+
+    def call(self):
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
