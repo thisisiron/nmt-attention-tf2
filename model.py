6458ad824cb40ed4bcb75a969c65d6d5b1871a8c
thisisiron <kimiron518@gmail.com>
2019-04-24 14:47:58 +0900

    add decoder and modify shape of computing

--- model.py
+++ model.py
@@ -16,7 +16,9 @@ class Encoder(tf.keras.Model):
         super(Encoder, self).__init__()
         self.batch_size = batch_size
         self.enc_units = units
+
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+
         self.lstm_1 = tf.keras.layers.LSTM(self.enc_units,
                                          return_sequences=True,
                                          return_state=True,
@@ -37,16 +39,17 @@ class Encoder(tf.keras.Model):
                                          return_state=True,
                                          recurrent_initializer='glorot_uniform')
 
-    def call(self, x):
+    def call(self, x, pre_state):
         x = self.embedding(x)
-        x, state_h_1, state_c_1 = self.lstm_1(x)
-        x, state_h_2, state_c_2 = self.lstm_2(x)
-        x, state_h_3, state_c_3 = self.lstm_3(x)
-        output, state_h_4, state_c_4 = self.lstm_4(x)
-        return output, [[state_h_1, state_c_1], [state_h_2, state_c_2], [state_h_3, state_c_3], [state_h_4, state_c_4]]
+        x, state_h_1, state_c_1 = self.lstm_1(x, initial_state=pre_state)
+        x, state_h_2, state_c_2 = self.lstm_2(x, initial_state=pre_state)
+        x, state_h_3, state_c_3 = self.lstm_3(x, initial_state=pre_state)
+        output, state_h_4, state_c_4 = self.lstm_4(x, initial_state=pre_state)
+        state = [[state_h_1, state_c_1], [state_h_2, state_c_2], [state_h_3, state_c_3], [state_h_4, state_c_4]]
+        return output, state
 
-    def initialize_hidden_state(self):
-        pass
+    def initialize_hidden_state(self, x):
+        return tf.random.uniform((self.batch_size, self.enc_units))
 
     def initialize_cell_state(self):
         return tf.zeros((self.batch_size, self.enc_units))
@@ -64,14 +67,17 @@ class Decoder(tf.keras.Model):
                                          return_sequences=True,
                                          return_state=True,
                                          recurrent_initializer='glorot_uniform')
+        
         self.lstm_2 = tf.keras.layers.LSTM(self.dec_units,
                                          return_sequences=True,
                                          return_state=True,
                                          recurrent_initializer='glorot_uniform')
+
         self.lstm_3 = tf.keras.layers.LSTM(self.dec_units,
                                          return_sequences=True,
                                          return_state=True,
                                          recurrent_initializer='glorot_uniform')
+
         self.lstm_4 = tf.keras.layers.LSTM(self.dec_units,
                                          return_sequences=True,
                                          return_state=True,
@@ -85,39 +91,57 @@ class Decoder(tf.keras.Model):
 
 
 
-    def call(self, x, init_state, enc_output, pre_h_t):
+    def call(self, x, pre_state, enc_output, pre_h_t):
         x = self.embedding(x)
-        x = tf.concat([x, pre_h_t], axis=1)
-        x, state_h_1, state_c_1 = self.lstm_1(x)
-        x, state_h_2, state_c_2 = self.lstm_2(x)
-        x, state_h_3, state_c_3 = self.lstm_3(x)
-        output, state_h_4, state_c_4 = self.lstm_4(x)
+
+        # input_feeding shape == (batch_size, 1, word_embedding_dim + pre_h_t_embedding_dim)
+        x = tf.concat([x, pre_h_t], axis=-1)
+        x, state_h_1, state_c_1 = self.lstm_1(x, initial_state=pre_state[0])
+        x, state_h_2, state_c_2 = self.lstm_2(x, initial_state=pre_state[1])
+        x, state_h_3, state_c_3 = self.lstm_3(x, initial_state=pre_state[2])
+
+        # dec_output shape == (batch_size, 1, units)
+        dec_output, state_h_4, state_c_4 = self.lstm_4(x, initial_state=pre_state[3])
         
         state = [[state_h_1, state_c_1], [state_h_2, state_c_2], [state_h_3, state_c_3], [state_h_4, state_c_4]]
 
         context_vector = self.attention_layer(dec_output, enc_output)
 
+        # h_t shape == (batch_size, embedding_dim)
         h_t = self.W_c(tf.concat([tf.expand_dims(context_vector, 1), dec_output], axis=-1))
 
+        # y_t shape == (batch_size, vocab_size)
         y_t = self.pred(h_t)
 
-        return 
+        return y_t, state, h_t 
 
 
 class AttentionLayer(tf.keras.Model):
     def __init__(self, units):
-        super(Attention, self).__init__()
+        super(AttentionLayer, self).__init__()
         self.W_a = tf.keras.layers.Dense(units)
         self.v_a = tf.keras.layers.Dense(1)
 
     def call(self, dec_h_t, enc_h_s):
-        score = self.v_a(tf.nn.tanh(self.W_a(tf.concat([dec_h_t, enc_h_s], axis=1))))
+
+        print('dec_h_t shape:', dec_h_t.shape)
+        print('enc_h_s shape:', enc_h_s.shape)
+        # dec_h_t shape == (batch_size, 1, units)
+        # enc_h_s shape == (batch_size, seq_len, units) 
+#        concat_h = tf.concat([dec_h_t, enc_h_s], axis=1)
+#        concat_h = tf.reshape(concat_h, [concat_h.shape[0] * concat_h.shape[1], concat_h.shape[2]])
+#        print('concat_h shape:', concat_h.shape)
+        score = self.v_a(tf.nn.tanh(self.W_a(dec_h_t + enc_h_s)))
+        print('score shape:', score.shape)
 
         # attention_weights shape == (batch_size, seq_len, 1)
         attention_weights = tf.nn.softmax(score, axis=1)
+        print('attention_weights shape:', attention_weights.shape)
 
-        # context_vector shape == (batch_size, hidden_size)
-        context_vector = tf.reduce_sum((attention_weights * enc_h_s), axis=1)
+        # context_vector shape == (batch_size, units)
+        context_vector = tf.reduce_sum(tf.matmul(attention_weights, enc_h_s, transpose_a=True), axis=1)
+
+        print('context_vector shape:', context_vector.shape)
 
         return context_vector
 
