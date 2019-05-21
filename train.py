8f6604718e2ffb9bc21cd2fa4447f4848028a248
Eon Kim <kimiron518@gmail.com>
2019-05-21 13:43:15 +0900

    modify loading data rows

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ def predict(args: Namespace):
 
 
 def train(args: Namespace):
-    input_tensor, target_tensor, input_lang_tokenizer, target_lang_tokenizer = load_dataset('./data/', args.max_len, limit_size=3000)
+    input_tensor, target_tensor, input_lang_tokenizer, target_lang_tokenizer = load_dataset('./data/', args.max_len, limit_size=None)
 
     max_len_input = len(input_tensor[0])
     max_len_target = len(target_tensor[0])
